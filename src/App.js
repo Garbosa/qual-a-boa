9c787456257ebba76c08a5a77bef854f8bc07baf
Garbosa <gabriel17000@gmail.com>
2022-05-15 23:31:04 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Qual a boa?</h1>
+      <button>INICIAR</button>
+    </div>
+  );
+}
+
+export default App;
